a88aeba2dd12d7e9aeb851eb9d1a93526f66ea8f
tombo09 <regular.tb@gmail.com>
2024-08-06 19:07:23 +0200

    c

--- partitions.sh
+++ partitions.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-
-
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <device>"
     exit 1
@@ -17,25 +15,56 @@ if [ "$encrypt" != "ja" ]; then
     exit 0
 fi
 
-echo "Erstellen der Partitionen..."
-parted $device -- mklabel gpt
-parted $device -- mkpart primary 1MiB 100%
+echo "Wie viele Partitionen möchten Sie innerhalb der verschlüsselten Festplatte erstellen?"
+read partition_count
 
-# Root-Partition verschlüsseln
-root_partition="${device}1"
+declare -a partition_sizes
+for ((i=1; i<$partition_count; i++))
+do
+    echo "Geben Sie die Größe der Partition $i in MiB an:"
+    read size
+    partition_sizes+=($size)
+done
 
+# Letzte Partition
+echo "Soll die letzte Partition den gesamten restlichen Speicherplatz verwenden? (ja/nein)"
+read use_rest
+
+if [ "$use_rest" != "ja" ]; then
+    echo "Geben Sie die Größe der letzten Partition in MiB an:"
+    read size
+    partition_sizes+=($size)
+fi
+
+# Verschlüssele die gesamte Festplatte
 echo "Verschlüsseln der gesamten Festplatte..."
-cryptsetup luksFormat $root_partition
-cryptsetup open $root_partition cryptroot
+cryptsetup luksFormat $device
+cryptsetup open $device cryptroot
 
-echo "Formatieren und Label zuweisen..."
-mkfs.ext4 /dev/mapper/cryptroot -L NIXROOT
+# Erstelle ein virtuelles Partitionierungsschema auf der verschlüsselten Festplatte
+pvcreate /dev/mapper/cryptroot
+vgcreate vgcrypt /dev/mapper/cryptroot
 
-echo "Mounten der Partition..."
-mount /dev/mapper/cryptroot /mnt
+start=1
+for ((i=1; i<$partition_count; i++))
+do
+    lvcreate -L ${partition_sizes[$i-1]}MiB -n lv$i vgcrypt
+done
 
-# Boot-Partition innerhalb der verschlüsselten Partition erstellen
+if [ "$use_rest" == "ja" ]; then
+    lvcreate -l 100%FREE -n lv$partition_count vgcrypt
+else
+    lvcreate -L ${partition_sizes[$partition_count-1]}MiB -n lv$partition_count vgcrypt
+fi
+
+# Formatieren und Label zuweisen
+mkfs.fat -F 32 /dev/vgcrypt/lv1
+fatlabel /dev/vgcrypt/lv1 NIXBOOT
+mkfs.ext4 /dev/vgcrypt/lv2 -L NIXROOT
+
+# Mounten der Partitionen
+mount /dev/vgcrypt/lv2 /mnt
 mkdir -p /mnt/boot
-mount ${device}p1 /mnt/boot
+mount /dev/vgcrypt/lv1 /mnt/boot
 
 echo "Partitionierung und Verschlüsselung abgeschlossen. Fortfahren mit der NixOS-Installation."
