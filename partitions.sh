77cc7c9d37dbda9f74e3d81960398fc268b66981
tombo09 <regular.tb@gmail.com>
2024-08-07 18:29:38 +0200

    c

--- partitions.sh
+++ partitions.sh
@@ -58,7 +58,14 @@ nixos-generate-config --root /mnt
 sudo cp /home/nixos/setup/configuration.nix /mnt/etc/nixos/configuration.nix
 
 
-sed -i '$e echo "  boot.initrd.luks.devices = {\n    root = {\n      device = \"/dev/nvme0n1p2\";\n      preLVM = true;\n    };\n  };"' /mnt/etc/nixos/configuration.nix
+
+sed -i '/^}/i\
+  boot.initrd.luks.devices = {\n\
+    root = {\n\
+      device = "/dev/nvme0n1p2";\n\
+      preLVM = true;\n\
+    };\n\
+  };' /mnt/etc/nixos/configuration.nix
 
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
 
